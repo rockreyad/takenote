e8eea50273a196953f6d3d5c552e965b1c5c1b35
maruffahmed <marufah76@gmail.com>
2023-07-08 05:05:24 +0600

    Use Inter font

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,13 @@ import { Analytics } from '@vercel/analytics/react';
 import Sidebar from '@/components/sidebar';
 import Header from '@/components/header';
 import ThemeProviderWrapper from '@/context/ThemeProvider';
+import { Inter } from 'next/font/google';
+
+// If loading a variable font, you don't need to specify the font weight
+const inter = Inter({
+  subsets: ['latin'],
+  display: 'swap'
+});
 
 export const metadata = {
   title: 'Next.js 13 + PlanetScale + NextAuth + Tailwind CSS',
@@ -18,9 +25,9 @@ export default async function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en" className="h-full bg-gray-50">
+    <html lang="en" className={`h-full bg-gray-50 ${inter.className}`}>
       <ThemeProviderWrapper>
-        <body className="h-full">
+        <body className="h-full font-sans">
           <DashboardLayoutProvider>
             <Sidebar />
             <div className="lg:pl-72">
