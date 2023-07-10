400927857bc12a77dd0bd91fea5ef0ac6e12c149
maruffahmed <marufah76@gmail.com>
2023-07-10 13:39:44 +0600

    Improve table, loading

--- app/files/[fileId]/loading.tsx
+++ app/files/[fileId]/loading.tsx
@@ -3,11 +3,11 @@ import { Loader2 } from 'lucide-react';
 export default function Loading() {
   return (
     <>
-      <div className="fixed z-50 inset-0 flex items-center justify-center">
+      <div className="fixed z-50 inset-0 flex flex-col gap-2 items-center justify-center bg-white lg:left-52">
         <div className="animate-spin">
           <Loader2 />
         </div>
-        <p>Loading...</p>
+        <p className="animate-pulse">loading..</p>
       </div>
     </>
   );
