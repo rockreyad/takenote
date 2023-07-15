5178349c1a017496ddb12cf948afb3ebc31d6186
maruffahmed <marufah76@gmail.com>
2023-07-16 03:07:34 +0600

    Add new sections

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,11 @@
 import { PrimaryFeatures } from '@/components/home/PrimaryFeatures';
+import Contact from '@/components/home/contact';
+import FAQ from '@/components/home/faq';
+import Features from '@/components/home/features';
+import Footer from '@/components/home/footer';
 import Hero from '@/components/home/hero';
 import LogoCloud from '@/components/home/logo-cloud';
+import NewsLetter from '@/components/home/newsletter';
 import { FunctionComponent } from 'react';
 
 interface HomeProps {}
@@ -11,6 +16,11 @@ const Home: FunctionComponent<HomeProps> = () => {
       <Hero />
       <LogoCloud />
       <PrimaryFeatures />
+      <Features />
+      <FAQ />
+      <Contact />
+      <NewsLetter />
+      <Footer />
     </>
   );
 };
