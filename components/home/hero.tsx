df81b1f0a95ebe5ba83bfc06636c71717a3517a9
rockreyad <rockreyad@gmail.com>
2023-07-17 21:31:22 +0600

    mouse pointer dark mode updated

--- components/home/hero.tsx
+++ components/home/hero.tsx
@@ -88,7 +88,7 @@ export default function Hero() {
   }, []);
   return (
     <div
-      className="bg-white dark:bg-gray-900 overflow-hidden relative before:pointer-events-none before:fixed before:inset-0 before:z-0 before:bg-[radial-gradient(circle_farthest-side_at_var(--x,_100px)_var(--y,_100px),#98FF24,_transparent_100%)] before:opacity-40"
+      className="bg-white dark:bg-gray-900 overflow-hidden relative before:pointer-events-none before:fixed before:inset-0 before:z-0 before:bg-[radial-gradient(circle_farthest-side_at_var(--x,_100px)_var(--y,_100px),#98FF24,_transparent_100%)] dark:before:bg-[radial-gradient(circle_farthest-side_at_var(--x,_100px)_var(--y,_100px),#7775D6,_transparent_100%)] before:opacity-40 dark:before:opacity-40"
       ref={main}
     >
       <Navbar />
