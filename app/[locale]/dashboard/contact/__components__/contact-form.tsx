33a2d640f11b5f0220ece4ae79a2044707b41deb
minhna123 <minhld1709@gmail.com>
2023-09-02 00:30:07 +0700

    [dashboard/contact] Use text stead input form to display name and email in user contact form

--- app/[locale]/dashboard/contact/__components__/contact-form.tsx
+++ app/[locale]/dashboard/contact/__components__/contact-form.tsx
@@ -12,17 +12,14 @@ export default function ContactForm() {
             <label className="block text-sm font-semibold leading-6 text-gray-900 dark:text-white " >
               Full name
             </label>
-            <div className="mt-2.5">
-              <input type="text" autoComplete="family-name" value={session?.user?.name as string}  className="block w-full rounded-md dark:bg-white/5 border-0 px-3.5 py-2 text-gray-900  dark:text-white  shadow-sm ring-1 ring-inset ring-gray-300 dark:ring-white/10 placeholder:text-gray-400 dark:placeholder:text-gray-500 focus:ring-2 focus:ring-inset focus:ring-primary/40 dark:focus:ring-primary/10 sm:text-sm sm:leading-6" />
-            </div>
+            {session?.user?.name as string}
           </div>
           <div className="sm:col-span-2">
             <label htmlFor="email" className="block text-sm font-semibold leading-6 text-gray-900 dark:text-white " >
               Email
             </label>
-            <div className="mt-2.5">
-              <input type="email" name="email" id="email" autoComplete="email" value={session?.user?.email as string} className="block w-full rounded-md dark:bg-white/5 border-0 px-3.5 py-2 text-gray-900  dark:text-white  shadow-sm ring-1 ring-inset ring-gray-300 dark:ring-white/10 placeholder:text-gray-400 dark:placeholder:text-gray-500 focus:ring-2 focus:ring-inset focus:ring-primary/40 dark:focus:ring-primary/10 sm:text-sm sm:leading-6" />
-            </div>
+            {session?.user?.email as string}
+            <input name="email" hidden value={session?.user?.email as string} />
           </div>
           <div className="sm:col-span-2">
             <label htmlFor="message" className="block text-sm font-semibold leading-6 text-gray-900 dark:text-white " >
