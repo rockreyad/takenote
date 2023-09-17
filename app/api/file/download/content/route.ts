853115cfceccf94413648d9c4f795db0d6eb1751
le dinh minh <minhld1709@users.noreply.github.com>
2023-09-18 03:11:52 +0700

    [DownloadPdf] Update color for SPEAKER (#71)

--- app/api/file/download/content/route.ts
+++ app/api/file/download/content/route.ts
@@ -7,6 +7,11 @@ export async function GET(request: NextRequest) {
   //get fileId from request
   const { searchParams } = new URL(request.url);
   const fileId = searchParams.get('fileId');
+  const speakerColor = (speaker: string) => {
+    if (speaker === 'SPEAKER_00') return '#0007F3';
+    if (speaker === 'SPEAKER_01') return '#FF7901';
+    return '#056F11';
+  }
 
   if (!fileId)
     return NextResponse.json({ error: 'File not found', status: 404 });
@@ -19,14 +24,18 @@ export async function GET(request: NextRequest) {
   const fileName = transcribe!.file!.name.split('.')[0];
 
   const content = `
-    <h2>TakeNote Transcription File</h2>
-    <p>File Name: ${transcribe!.file!.name}</p>
-    <p>Creation Date: ${transcribe!.file!.createdAt}</p>
-    <p>User Name: ${transcribe!.file!.user!.name}</p>
+    <div style="color: #4472c4;">
+      <h2>TakeNote Transcription File</h2>
+      <p><span style="font-weight: bold;">File Name:</span> <span>${transcribe!.file!.name}</span></p>
+      <p><span style="font-weight: bold;">Creation Date:</span> <span>${transcribe!.file!.createdAt}</span></p>
+      <p><span style="font-weight: bold;">User Name:</span> <span>${transcribe!.file!.user!.name}</span></p>
+    </div>
     <br>
+
     <h3>Transcript</h3>
     <p>${transcribe!.transcript}</p>
     <br>
+
     <h3>Speaker Diarization</h3>
     `;
 
@@ -37,10 +46,13 @@ export async function GET(request: NextRequest) {
       const startTime = formatTimeFromSeconds(entry.start);
       const speakerText = entry.text;
       return [
-        `<div>${speakerId}    ${startTime}</div>
-    <div><p>${speakerText}</p></div>
-    <br>
-  `];
+        `<div>
+          <div><span style="color: ${speakerColor(speakerId)}; margin-right: 15px;">${speakerId}</span>    <span>${startTime}</span></div>
+          <div><span>${speakerText}</span></div>
+          <hr style="border-width: 3px; margin-top: 15px;">
+        </div>
+        <br>
+      `];
     }).join('');
 
   return NextResponse.json({
