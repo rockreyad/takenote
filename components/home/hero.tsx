926ab5b0a11cbca32eb16b92ec8ab097f9ef85db
Shaheerazam111 <shaheerazam111@gmail.com>
2023-09-17 15:36:17 +0500

    Register button Issue fixed

--- components/home/hero.tsx
+++ components/home/hero.tsx
@@ -116,7 +116,7 @@ export default function Hero() {
               </p>
               <div className="mt-10 flex items-center justify-center gap-x-6">
                 <a
-                  href="#"
+                  href="/auth/signin"
                   className="rounded-md bg-green-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-green-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-green-600"
                 >
                   Register for free
